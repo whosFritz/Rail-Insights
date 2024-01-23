afd84128f8e3e6e2a0ada9063d4d2c921b23c5d4
Fritz Schubert <fritzschubert21@outlook.de>
2024-01-23 12:05:18 +0100

    init commit

---
+++ src/main/java/de/whosfritz/RailMetrics/RailMetricsApplication.java
@@ -0,0 +1,13 @@
+package de.whosfritz.RailMetrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailMetricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailMetricsApplication.class, args);
+	}
+
+}
