c0ead91e937e50bca2b7f32cc2768ef9ccf7ff12
olech2412 <olechristoph2412@gmail.com>
2024-01-23 19:49:10 +0100

    added EntityScan to dbadapter models

--- src/main/java/de/whosfritz/railmetrics/RailMetricsApplication.java
+++ src/main/java/de/whosfritz/railmetrics/RailMetricsApplication.java
@@ -2,8 +2,10 @@ package de.whosfritz.railmetrics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"de.olech2412.adapter.dbadapter.model"}) // scan JPA entities from the dbadapter
 public class RailMetricsApplication {
 
     public static void main(String[] args) {
