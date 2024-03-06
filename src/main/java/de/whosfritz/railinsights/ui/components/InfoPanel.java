1adb1550b21dbf958d308ca27ede375b6da5955d
olech2412 <olechristoph2412@gmail.com>
2024-03-06 15:42:50 +0100

    removed confusing text from homeview -> just dont tell the user

--- src/main/java/de/whosfritz/railinsights/ui/components/InfoPanel.java
+++ src/main/java/de/whosfritz/railinsights/ui/components/InfoPanel.java
@@ -10,8 +10,7 @@ public class InfoPanel extends VerticalLayout {
         Paragraph welcomeText = new Paragraph("Hey! Herzlich Willkommen bei RailInsights. Hier findest du " +
                 "interessante Statistiken und Daten rund um die Deutsche Bahn. Viel Spaß! :)");
         Paragraph infoText = new Paragraph("Die Daten werden alle 15 Minuten aktualisiert für eine Stunde " +
-                "vorausschauend. Damit du ein möglichst flüssiges Ergebnis in der Navigation hast, aktualisieren wir " +
-                "die Daten, in der Benutzeroberfläche, nur alle 20 Minuten. Ob du gerade einen aktuellen Stand siehst oder " +
+                "vorausschauend. Ob du gerade einen aktuellen Stand siehst oder " +
                 "gerade neue Daten geladen werden siehst du oben rechts an dem angezeigten Icon. Durch aktualisieren " +
                 "der Seite, kannst du immer prüfen, ob du auf dem aktuellen Stand bist. In dem Navigationsmenü auf der " +
                 "linken Seite findest du verschiedene Bereiche, die du dir anschauen kannst. Viel Spaß beim Erkunden!");
